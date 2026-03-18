76fa95ae001e22cbf86c5e1cb5bc5fe2d1627bc6
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-18 16:12:06 +0100

    Add HTML structure

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scrivere un programma che chieda all’utente:
+//- Il numero di chilometri da percorrere
+
+
+// - Età del passeggero
+
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+
+
+
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65
+
+
